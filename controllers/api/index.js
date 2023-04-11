5ebfa594e592381f3585d0ceaf775be777f687c6
Aboubacar7 <abdrago17@yahoo.com>
2023-04-10 20:15:26 -0700

    adding selected space page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const userLogin = require('./userLogin.js')
+const selectedSpace = require('./selected-space');
 
 router.use('/users', userLogin)
-
+router.use('/select', selectedSpace)
 module.exports = router
