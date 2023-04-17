4029baf9fc1fd6026db2191d7c424639f734325b
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-04-17 14:54:31 -0700

    connect to jasdb

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 let sequelize;
 
-if(process.env.JAWSDB_URL){
-    sequelize = new Sequelize(process.env.JAWSDB_URL)
-} else{
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -14,8 +14,8 @@ if(process.env.JAWSDB_URL){
             host: 'localhost',
             dialect: 'mysql',
             port: 3306,
-            logging: false
-        }
+            logging: true
+        },
     );
 }
 
