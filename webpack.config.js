34ef3a4ae6fd75b89eb3af1e1bc02da5f1a2d45b
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-04-19 18:38:28 -0700

    re-added the script after adding some npm_config_loglevel and npm_config_production variables within heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: "./public/js/calendar.js",
-  output: {
-    filename: 'calendarBundle.js',
-    path: path.resolve(__dirname, 'public/js/'),
-    publicPath: '/js/'
-  },
-  mode: 'production',
-};
+// module.exports = {
+//   entry: "./public/js/calendar.js",
+//   output: {
+//     filename: 'calendarBundle.js',
+//     path: path.resolve(__dirname, 'public/js/'),
+//     publicPath: '/js/'
+//   },
+//   mode: 'production',
+// };
